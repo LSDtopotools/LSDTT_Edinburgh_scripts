4b001f9e3b733845206cbadc8a5f24a71eacb914
simon-m-mudd <simon.m.mudd@ed.ac.uk>
2019-08-30 09:44:39 +0100

    First commit

---
+++ LSDMT_UEdin_setup.sh
@@ -0,0 +1,4 @@
+# This script is for setting up LSDMappingTools on a university of edinbirgh server
+echo Welcome to the tool for setting up LSDMappingTools on
+echo a University of Edinbirgh School of GeoSciences server
+echo This script has the side effect of getting you ready to set up any python environment using conda
---
+++ LSDTT_UEdin_setup.sh
@@ -0,0 +1,10 @@
+# This is a script for setting up LSDTopoTools for a student or researcher at the University of Edinburgh
+echo =====================================================================
+echo Welcome to the Univerty of Edinburgh's LSDTopoTools setup script
+echo I am going to create some directories, 
+echo grab the code and data, make the programs, 
+echo and in general get everything set up as painlessly as possible. 
+echo Let's get started. Can you please enter your university user name?
+echo If you get this wrong nothng will work so double check before you hit enter.
+
+read -p 'University of Edinburgh User name (UUN): ' uunvar
